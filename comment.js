bbebab12fbabd83117b33d58382c07f4bb3bc167
Dmitry Satsura <dimasatsura@gmail.com>
2021-10-28 15:29:42 +0300

    comment 2

--- comment.js
+++ comment.js
@@ -1,22 +1,25 @@
 var price = 28.99;//создание переменой price и присваивание ей значение 28.99
 var discount = 10;// создание переменной discount и присваивание ей значения 10 
 var total = // создание переменной total 
-    price - (price * (discount / 100)); 
-if (total > 25) {
-    freeShipping(); 
-}
+    price - (price * (discount / 100));//присваивание переменной total результатов вычисления 
+if (total > 25) { /*сравнение переменной total and 25,
+     если total больше то выполняется freeShipping*/ 
+    freeShipping();//выполнениие кода  
+} // конец команды if
 
-var count = 10;
-while (count > 0) {
-    juggle();
-count = count - 1; }
-var dog = {name: "Rover", weight: 35}; 
-    if (dog.weight > 30) {
-alert("WOOF WOOF"); } 
-    else {
-    alert("woof woof"); 
-}
+var count = 10;// присвайвание переменной count значения 10 
+while (count > 0) {// count больше нуля й   
+    juggle();// выполнение
+count = count - 1;// count - 1 
+ }// конец команы while
 
-var circleRadius = 20;
-var circleArea =
-Math.PI * (circleRadius * circleRadius);
+var dog = {name: "Rover", weight: 35};// создание переменной dog с атрибутами name and weight 
+    if (dog.weight > 30) {// если атрибут weight больше 30
+alert("WOOF WOOF"); }// то WOOF WOOF  
+    else {// если меньше 30
+    alert("woof woof");// то woof woof
+}// конец команыды if
+
+var circleRadius = 20;// присванивание переменной значения 20
+var circleArea = // создание переменной  
+Math.PI * (circleRadius * circleRadius);// и привавание ей вычисления 
