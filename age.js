d1221683359ce73b60b6ad1ba509b4e3fc677fd9
Dmitry Satsura <dimasatsura@gmail.com>
2021-10-28 15:44:15 +0300

    age

---
+++ age.js
@@ -0,0 +1,9 @@
+let age = 25;
+let name = "Dmitry"
+function ageName();{
+    if (age >17) {
+        aletr ("Простите но эта страница не для детей")
+    } else {
+        alett ("Добро пожаловать" + "name"+ "!")
+    }
+}   
